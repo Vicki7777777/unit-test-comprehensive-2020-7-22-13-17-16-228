cefb55c5c36357dfd211c3eeeb8a6ec78e120eff
Vicki <458044962qq.com>
2020-07-23 21:02:46 +0800

    [GuessNumberGame][Vicki]Test:should_return_4A0B_when_Answer_is_1234_GuessNumber_given_1234

---
+++ src/main/java/GuessNumberGame.java
@@ -0,0 +1,10 @@
+public class GuessNumberGame{
+
+    public String guessResult(String guestNumber) {
+
+        return "4A0B";
+    }
+
+
+}
+
---
+++ src/test/java/GuessNumberGameTest.java
@@ -0,0 +1,16 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class GuessNumberGameTest {
+
+    @Test
+    void should_return_4A0B_when_Answer_is_1234_GuessNumber_given_1234() {
+        //given
+        String guestNumber = "1 2 3 4";
+        //when
+        GuessNumberGame guessNumberGame = new GuessNumberGame();
+        String actual = guessNumberGame.guessResult(guestNumber);
+        //then
+        Assertions.assertEquals("4A0B",actual);
+    }
+}
