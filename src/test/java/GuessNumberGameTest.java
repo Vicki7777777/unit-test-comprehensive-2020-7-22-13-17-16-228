7c22fae7b263ede4c973aa9d47b3569717d4ce4c
Vicki <458044962qq.com>
2020-07-23 21:43:23 +0800

    [GuessNumberGame][Vicki]Test:should_return_1A3B_when_Answer_is_3124_GuessNumber_given_1234 test

--- src/test/java/GuessNumberGameTest.java
+++ src/test/java/GuessNumberGameTest.java
@@ -13,5 +13,15 @@ public class GuessNumberGameTest {
         //then
         Assertions.assertEquals("4A0B",actual);
     }
+    @Test
+    void should_return_1A3B_when_Answer_is_3124_GuessNumber_given_1234() {
+        //given
+        String guestNumber = "3 1 2 4";
+        //when
+        GuessNumberGame guessNumberGame = new GuessNumberGame();
+        String actual = guessNumberGame.guessResult(guestNumber);
+        //then
+        Assertions.assertEquals("1A3B",actual);
+    }
 
 }
