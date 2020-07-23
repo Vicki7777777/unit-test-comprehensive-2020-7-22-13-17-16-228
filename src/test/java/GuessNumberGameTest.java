78a07fda2129b3b3b516badba1fba19b47e2450a
Vicki <458044962qq.com>
2020-07-23 21:56:51 +0800

    [GuessNumberGame][Vicki]Test:finished “should_return_1A3B_when_Answer_is_3124_GuessNumber_given_1234”、“should_return_1A1B_when_Answer_is_1562_GuessNumber_given_1234”、“should_return_0A2B_when_Answer_is_3456_GuessNumber_given_1234”、“should_return_0A0B_when_Answer_is_5678_GuessNumber_given_1234” test

--- src/test/java/GuessNumberGameTest.java
+++ src/test/java/GuessNumberGameTest.java
@@ -34,5 +34,36 @@ public class GuessNumberGameTest {
         //then
         Assertions.assertEquals("1A3B",actual);
     }
+    @Test
+    void should_return_1A1B_when_Answer_is_1562_GuessNumber_given_1234() {
+        //given
+        String guestNumber = "1 5 6 2";
+        //when
+        GuessNumberGame guessNumberGame = new GuessNumberGame();
+        String actual = guessNumberGame.guessResult(guestNumber);
+        //then
+        Assertions.assertEquals("1A1B",actual);
+    }
+    @Test
+    void should_return_0A2B_when_Answer_is_3456_GuessNumber_given_1234() {
+        //given
+        String guestNumber = "3 4 5 6";
+        //when
+        GuessNumberGame guessNumberGame = new GuessNumberGame();
+        String actual = guessNumberGame.guessResult(guestNumber);
+        //then
+        Assertions.assertEquals("0A2B",actual);
+    }
+
+    @Test
+    void should_return_0A0B_when_Answer_is_5678_GuessNumber_given_1234() {
+        //given
+        String guestNumber = "5 6 7 8";
+        //when
+        GuessNumberGame guessNumberGame = new GuessNumberGame();
+        String actual = guessNumberGame.guessResult(guestNumber);
+        //then
+        Assertions.assertEquals("0A0B",actual);
+    }
 
 }
