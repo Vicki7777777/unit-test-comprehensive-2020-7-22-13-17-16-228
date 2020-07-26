1f2eadcf9eb6eb3287e07bd14a11b5572ac1c6ad
Vicki <458044962qq.com>
2020-07-26 21:21:16 +0800

    [GuessNumberGames][Vicki]Feature:implement should_return_wrongMessage_when_inputWrong_given_repeatInput

--- src/main/java/GuessNumberGame/VaildOfInput.java
+++ src/main/java/GuessNumberGame/VaildOfInput.java
@@ -1,9 +1,20 @@
 package GuessNumberGame;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class VaildOfInput {
 
     public boolean isVaild(String[] input) {
         boolean vaild = true;
+        Set<String> vaildSet = new HashSet<String>();
+        for(int i=0;i<input.length;i++){
+            vaildSet.add(input[i]);
+        }
+        int i = vaildSet.size();
+        if (vaildSet.size() != input.length){
+            vaild = false;
+        }
         return vaild;
     }
 }
