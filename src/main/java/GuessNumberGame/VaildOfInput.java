090ff0cf09b5ae94451b66b341b1c116a6cfc6a1
Vicki <458044962qq.com>
2020-07-26 21:37:00 +0800

    [GuessNumberGames][Vicki]Feature:implement should_return_false_when_input_given_onlyTwoNumber

--- src/main/java/GuessNumberGame/VaildOfInput.java
+++ src/main/java/GuessNumberGame/VaildOfInput.java
@@ -8,9 +8,9 @@ public class VaildOfInput {
     public boolean isVaild(String[] input) {
         boolean vaild = true;
         Set<String> vaildSet = new HashSet<String>();
-        if(input == null){
+        if(input == null || (input.length != 4)){
             vaild = false;
-        }else{
+        } else{
             for(int i=0;i<input.length;i++){
                 vaildSet.add(input[i]);
             }
