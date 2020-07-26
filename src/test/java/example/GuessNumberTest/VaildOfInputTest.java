8d9bf972b595d185f4bb4cc6100e69261f873702
Vicki <458044962qq.com>
2020-07-26 21:39:12 +0800

    [GuessNumberGames][Vicki]Test+Feature: Add and implement should_return_false_when_input_given_fiveNumber test

--- src/test/java/example/GuessNumberTest/VaildOfInputTest.java
+++ src/test/java/example/GuessNumberTest/VaildOfInputTest.java
@@ -4,6 +4,7 @@ import GuessNumberGame.VaildOfInput;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class VaildOfInputTest {
     @Test
@@ -19,12 +20,12 @@ public class VaildOfInputTest {
     @Test
     void should_return_true_when_input_given_rightInput(){
         //given
-        String[] input = {"1","2","3","3"};
+        String[] input = {"1","2","3","4"};
         VaildOfInput vaildOfInput = new VaildOfInput();
         //when
         boolean flag = vaildOfInput.isVaild(input);
         //then
-        assertFalse(flag);
+        assertTrue(flag);
     }
     @Test
     void should_return_false_when_input_given_nullInput(){
@@ -40,6 +41,16 @@ public class VaildOfInputTest {
     void should_return_false_when_input_given_onlyTwoNumber(){
         //given
         String[] input = {"1","2"};
+        VaildOfInput vaildOfInput = new VaildOfInput();
+        //when
+        boolean flag = vaildOfInput.isVaild(input);
+        //then
+        assertFalse(flag);
+    }
+    @Test
+    void should_return_false_when_input_given_fiveNumber(){
+        //given
+        String[] input = {"1","2","3","4","5"};
         VaildOfInput vaildOfInput = new VaildOfInput();
         //when
         boolean flag = vaildOfInput.isVaild(input);
